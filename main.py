669014d549a67b5aa27103cef6ff6be7df0c5316
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2017-12-12 23:02:42 +0100

    Add form and endpoint to add new comments

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from collections import namedtuple
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 Comment = namedtuple('Comment', ['title', 'content'])
-
-@app.route('/')
-def index():
-    comments = [
+comments = [
         Comment("First comment", "This page isn't bad at all."),
         Comment("Second comment", "Hi there. Lets start the party!"),
-        ...
         ]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        global comments
+        comment_title = request.form['title']
+        comment_content = request.form['content']
+        comments.append(Comment(comment_title, comment_content))
+
     return render_template('index.html', comments=comments)
