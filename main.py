f7a2692b5fe734e9efeebf4923b0c9434696b542
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2018-11-05 12:02:54 +0100

    Update project for Feria del Ingenio demo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import emoji
 import telepot
 from flask import Flask, render_template, request
 from flask_ini import FlaskIni
+from flask_socketio import SocketIO, emit
 from telepot.delegate import create_open, pave_event_space, per_chat_id
 from telepot.loop import MessageLoop
 
@@ -12,6 +13,7 @@ with app.app_context():
     app.iniconfig = FlaskIni()
     app.iniconfig.read('config.ini')
 
+socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,7 +24,18 @@ def index():
         create_comment(comment_title, comment_content)
 
     comments = get_comments()
-    return render_template('index.html', comments=comments)
+    return render_template('index.html', comments=comments, domain=app.iniconfig.get('flask', 'domain'))
+
+
+@socketio.on('new_comment')
+def new_comment(comment):
+    comment_title = comment['title']
+    comment_content = comment['content']
+    emit('new_comment', {
+        'title': comment_title,
+        'content': comment_content
+    }, broadcast=True)
+    create_comment(comment_title, comment_content)
 
 
 class MessageCounter(telepot.helper.ChatHandler):
@@ -31,11 +44,18 @@ class MessageCounter(telepot.helper.ChatHandler):
         super(MessageCounter, self).__init__(*args, **kwargs)
 
     def on_chat_message(self, msg):
-        if not 'text' in msg or not msg['text'].startswith('/newcomment'):
+        if 'text' in msg and msg['text'].startswith('/newcomment'):
+            self.handle_text_message(msg)
+        else:
+            self.sender.sendMessage(emoji.emojize("Sorry, but I can't understand you! :confused:", use_aliases=True))
+
+    def handle_text_message(self, msg):
+        text = msg['text'].strip()
+        
+        if ' ' not in text: # Empty message
             self.sender.sendMessage(emoji.emojize("Sorry, but I can't understand you! :confused:", use_aliases=True))
             return
 
-        text = msg['text'].lstrip()
         _, comment = text.split(' ', 1)             # Discard command
         title, *content = comment.split('\n', 1)    # Split comment members
 
@@ -50,21 +70,32 @@ class MessageCounter(telepot.helper.ChatHandler):
 
         create_comment(title, content)
 
+        emit('new_comment', {
+            'title': title,
+            'content': content
+        }, broadcast=True)
+
         self.sender.sendMessage(emoji.emojize("Comment added to guestbook. :grin:", use_aliases=True))
 
 
-bot = telepot.DelegatorBot(app.iniconfig.get('telegram', 'token'), [
-    pave_event_space()(
-        per_chat_id(), create_open, MessageCounter, timeout=10
-    ),
-])
+try:
+    bot = telepot.DelegatorBot(app.iniconfig.get('telegram', 'token'), [
+        pave_event_space()(
+            per_chat_id(), create_open, MessageCounter, timeout=10
+        ),
+    ])
+    
+    # Discard Telegram updates when bot was offline
+    updates = bot.getUpdates()
+    
+    if updates:
+        last_update_id = updates[-1]['update_id']
+        bot.getUpdates(offset=last_update_id + 1)
+    
+    # Run telepot bot
+    MessageLoop(bot).run_as_thread()
+except telepot.exception.TelegramError:
+    import sys
+    print('Sth wrong happens when trying to run telegram bot', file=sys.stderr)
 
-# Discard Telegram updates when bot was offline
-updates = bot.getUpdates()
-
-if updates:
-    last_update_id = updates[-1]['update_id']
-    bot.getUpdates(offset=last_update_id + 1)
-
-# Run telepot bot
-MessageLoop(bot).run_as_thread()
+socketio.run(app, host=app.iniconfig.get('flask', 'host'), port=app.iniconfig.getint('flask', 'port'))
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 # Load virtual environment
-source venv/bin/activate
+# source venv/bin/activate
+# pipenv shell
 
 # Run Flask application
 export FLASK_APP=main.py
-flask run
+flask run --host 0.0.0.0 --port 8080
 
-deactivate
+# deactivate
+# exit
